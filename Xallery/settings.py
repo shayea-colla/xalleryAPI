04ebaff5759aa3f43676637e9aa250c8e41d4564
solomon <samysam204o@gmail.com>
2023-11-06 06:25:09 +0300

    changed postgresql database to sqlite

--- Xallery/settings.py
+++ Xallery/settings.py
@@ -89,15 +89,11 @@ WSGI_APPLICATION = "Xallery.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.postgresql",
-        "NAME": "xallery_db",
-        "HOST": "127.0.0.1",
-        "PORT": "5432",
-        "USER": "solo",
-        "PASSWORD": "no way home ",
 
+DATABASES = { 
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
     }
 }
 
