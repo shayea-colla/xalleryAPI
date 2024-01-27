de8faadfc98ac9c8579c2a90b2a6bb58ebb741df
solomon <samysam204o@gmail.com>
2024-01-28 01:23:38 +0300

    first tags commit

--- core/mixins.py
+++ core/mixins.py
@@ -2,6 +2,10 @@ from .debug import debug
 
 
 class SetOwnerTheCurrentUserMixin:
+    """
+    set the owner of the object to the current user
+    """
+
     def create(self, validated_data):
         """Assume the object has owner field"""
         user = self.context["request"].user
